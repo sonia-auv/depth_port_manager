fc294ee1dc02161ca767d8bdaa497779a46fa072
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2025-07-21 15:11:47 -0400

    open port independent from node

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,20 @@ int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     // Device and connection defined by env var.
-    depth_port_manager::ImpactSubsea device;
     sonia_common_cpp::SerialConn conn("/dev/DEPTH", B115200, true);
-    auto depth = std::make_shared<depth_port_manager::DepthProvider>(device, conn);
-
-    if (!depth->OpenPort())
+    
+    if (!conn.OpenPort())
     {
         RCLCPP_FATAL(rclcpp::get_logger("depth_port_manager"), "Could not open port...");
         return EXIT_FAILURE;
     }
+
+    depth_port_manager::ImpactSubsea device;
+    auto depth = std::make_shared<depth_port_manager::DepthProvider>(device, conn);
+    
     rclcpp::spin(depth);
+    
     rclcpp::shutdown();
+    
     return EXIT_SUCCESS;
 }
