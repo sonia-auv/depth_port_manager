fa1a7180bc1a0af2194c0879a8f90902cc695d36
Erwanmanach <Manchoteu@aol.com>
2026-01-17 18:13:27 -0500

    clean up code

--- src/MS5837.cpp
+++ src/MS5837.cpp
@@ -179,42 +179,6 @@ namespace depth_port_manager
 
     float MS5837::altitude() { return (1 - pow((pressure() / 1013.25), .190284)) * 145366.45 * .3048; }
 
-
-    // uint8_t MS5837::crc4(uint16_t n_prom[])
-    // {
-    //     uint16_t n_rem = 0;
-
-    //     n_prom[0] = ((n_prom[0]) & 0x0FFF);
-    //     n_prom[7] = 0;
-
-    //     for (uint8_t i = 0; i < 16; i++)
-    //     {
-    //         if (i % 2 == 1)
-    //         {
-    //             n_rem ^= (uint16_t)((n_prom[i >> 1]) & 0x00FF);
-    //         }
-    //         else
-    //         {
-    //             n_rem ^= (uint16_t)(n_prom[i >> 1] >> 8);
-    //         }
-    //         for (uint8_t n_bit = 8; n_bit > 0; n_bit--)
-    //         {
-    //             if (n_rem & 0x8000)
-    //             {
-    //                 n_rem = (n_rem << 1) ^ 0x3000;
-    //             }
-    //             else
-    //             {
-    //                 n_rem = (n_rem << 1);
-    //             }
-    //         }
-    //     }
-
-    //     n_rem = ((n_rem >> 12) & 0x000F);
-
-    //     return n_rem ^ 0x00;
-    // }
-
     uint8_t MS5837::crc4(uint16_t n_prom[])  // n_prom defined as 8x unsigned int (n_prom[8])
     {
         int cnt;                 // simple counter
